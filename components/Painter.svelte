6f6d32990b8d52b09080d3414aeb1bf6617aeac1
miily8310s <rillakerika@gmail.com>
2021-06-07 08:36:47 +0900

    Add Prettier and format code

--- components/Painter.svelte
+++ components/Painter.svelte
@@ -1,107 +1,113 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  let sizeEL
-  let canvas
-  let size = 10
-  let isPressed = false
-  let color = 'black'
-  let x
-  let y
-  let ctx
+import { onMount } from "svelte";
+let sizeEL;
+let canvas;
+let size = 10;
+let isPressed = false;
+let color = "black";
+let x;
+let y;
+let ctx;
 
-  onMount(() => {
-    ctx = canvas.getContext('2d');
-    sizeEL.innerText = size
-  })
+onMount(() => {
+  ctx = canvas.getContext("2d");
+  sizeEL.innerText = size;
+});
 
-  
 function onCanvasMouseDown(e) {
-    isPressed = true
+  isPressed = true;
 
-    x = e.offsetX
-    y = e.offsetY
+  x = e.offsetX;
+  y = e.offsetY;
 }
 
 function onCanvasMouseUp(e) {
-    isPressed = false
+  isPressed = false;
 
-    x = undefined
-    y = undefined
+  x = undefined;
+  y = undefined;
 }
 
 function onCanvasMouseMove(e) {
-    if(isPressed) {
-        const x2 = e.offsetX
-        const y2 = e.offsetY
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
 
-        drawCircle(x2, y2)
-        drawLine(x, y, x2, y2)
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
 
-        x = x2
-        y = y2
-    }
+    x = x2;
+    y = y2;
+  }
 }
 
-  
-  function drawCircle(x, y) {
-      ctx.beginPath();
-      ctx.arc(x, y, size, 0, Math.PI * 2)
-      ctx.fillStyle = color
-      ctx.fill()
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
+  ctx.stroke();
+}
+
+function updateSizeOnScreen() {
+  sizeEL.innerText = size;
+}
+
+function onClickIncrease() {
+  size += 5;
+
+  if (size > 50) {
+    size = 50;
   }
-  
-  function drawLine(x1, y1, x2, y2) {
-      ctx.beginPath()
-      ctx.moveTo(x1, y1)
-      ctx.lineTo(x2, y2)
-      ctx.strokeStyle = color
-      ctx.lineWidth = size * 2
-      ctx.stroke()
+
+  updateSizeOnScreen();
+}
+
+function onClickDecrease() {
+  size -= 5;
+
+  if (size < 5) {
+    size = 5;
   }
 
-  function updateSizeOnScreen() {
-        sizeEL.innerText = size
-    }
-      
-    function onClickIncrease() {
-      size += 5
-  
-      if(size > 50) {
-          size = 50
-      }
-  
-      updateSizeOnScreen()
-    }
-    
-    function onClickDecrease() {
-      size -= 5
-  
-      if(size < 5) {
-          size = 5
-      }
-  
-      updateSizeOnScreen()
-    }
+  updateSizeOnScreen();
+}
 
-        function onClickClear() {
-      ctx.clearRect(0,0, canvas.width, canvas.height)
-    }
-
+function onClickClear() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 </script>
 
 <main>
-<canvas bind:this={canvas} on:mousedown={onCanvasMouseDown} on:mouseup={onCanvasMouseUp} on:mousemove={onCanvasMouseMove} id="canvas" width="800" height="700"></canvas>
-<div class="toolbox">
-  <button id="decrease" on:click={onClickDecrease}>-</button>
-  <span id="size" bind:this={sizeEL}></span>
-  <button id="increase" on:click={onClickIncrease}>+</button>
-  <input type="color" id="color" bind:value={color} on:change={ (e) => color = e.target.value}>
-  <button id="clear" on:click={onClickClear}>Clear</button>
-</div>
+  <canvas
+    bind:this={canvas}
+    on:mousedown={onCanvasMouseDown}
+    on:mouseup={onCanvasMouseUp}
+    on:mousemove={onCanvasMouseMove}
+    width="800"
+    height="700" />
+  <div class="toolbox">
+    <button on:click={onClickDecrease}>-</button>
+    <span bind:this={sizeEL} />
+    <button on:click={onClickIncrease}>+</button>
+    <input
+      type="color"
+      bind:value={color}
+      on:change={(e) => (color = e.target.value)} />
+    <button id="clear" on:click={onClickClear}>Clear</button>
+  </div>
 </main>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 
 * {
   box-sizing: border-box;
@@ -111,7 +117,7 @@ function onCanvasMouseMove(e) {
 
 main {
   background-color: white;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   display: flex;
   flex-direction: column;
   align-items: center;
